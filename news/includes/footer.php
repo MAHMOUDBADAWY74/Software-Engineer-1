bbe8238a45db6a6623264cb33c4e24195a7ff845
MAHMOUDBADAWY74 <105337014+MAHMOUDBADAWY74@users.noreply.github.com>
2023-05-11 23:25:27 +0300

    my project

---
+++ news/includes/footer.php
@@ -0,0 +1,28 @@
+
+             <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+        <script type="text/javascript">
+        function googleTranslateElementInit() {
+          new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+        }
+        </script>
+        <style>
+            .goog-logo-link {
+                display:none !important;
+            } 
+                
+            .goog-te-gadget{
+                color: transparent;
+            }
+            .goog-te-gadget .goog-te-combo {
+                margin: 0px 0;
+                padding: 8px;
+                color: #000;
+                background: #eeee;
+            }
+            #google_translate_element{
+                padding-top: 13px;
+                position: absolute;
+                top: 7px;
+                right:100px;
+            }
+        </style>
